328ccef853d8ef533cebb659a03a49205e574f65
ArkesteijnGijs <arkesteijngijs@gmail.com>
2021-05-09 12:44:54 +0200

    Test aangemaakt

---
+++ src/com/example/Recept.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import java.util.ArrayList;
+
+public class Recept {
+    private String naam;
+    private String ingredienten;
+    private String bereidingswijze;
+
+    public Recept(String naam, String ingredienten, String bereidingswijze){
+        this.naam=naam;
+        this.ingredienten=ingredienten;
+        this.bereidingswijze=bereidingswijze;
+    }
+    public String receptAanpassen(String naam, String ingredienten, String bereidingswijze){
+
+    }
+}
