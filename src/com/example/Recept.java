4509414d6284c0e58b021338c083f89e9ff79471
ArkesteijnGijs <arkesteijngijs@gmail.com>
2021-05-09 12:46:54 +0200

    Methode aangemaakt

--- src/com/example/Recept.java
+++ src/com/example/Recept.java
@@ -13,6 +13,9 @@ public class Recept {
         this.bereidingswijze=bereidingswijze;
     }
     public String receptAanpassen(String naam, String ingredienten, String bereidingswijze){
-
+        this.naam=naam;
+        this.ingredienten=ingredienten;
+        this.bereidingswijze=bereidingswijze;
+        return naam;
     }
 }
